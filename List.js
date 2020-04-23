c5f2d20a0e0dcf1d7ee8d84aa547ec714c7f9bc7
chinmey srivastav <srichinmey.43@gmail.com>
2020-04-23 13:25:21 +0530

    Add files via upload

---
+++ List.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Text,View } from 'react-native';
+
+ function Title() {
+  
+  
+
+  
+  return (
+    <View >
+    
+      <Text>hi</Text>
+    </View>
+  );
+}
+export default Title
+
+// return (
+//   <View style={styles.container}>
+//     <FlatList 
+    
+//     data={people}
+//     renderItem={({item})=>{
+//       return(
+//         <Text style={styles.input}>{item.name}</Text>
+//       )
+//     }}/>
+//   {/* <ScrollView>
+//     {
+//       people.map((item)=> {
+//         return (
+//           <View key={item.key}>
+//             <Text style={styles.input}>{item.name}</Text>
+//           </View>
+//         )
+//       }
+
+//       )
+//     }
+//     </ScrollView> */}
+//   </View>
+// );
+// }
+
